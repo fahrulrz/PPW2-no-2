21bf56c57357a946f39c1e96b0dd6e86aa053980
fahrulrz <mfahrulrazi695@gmail.com>
2024-10-08 15:09:00 +0700

    mebambah soal no 2

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+   
+    protected $fillable = [
+        'name', 
+        'code',
+        'name',
+        'quantity',
+        'price',
+        'description',
+        'updated_at',
+        'created_at',
+
+    ];
 }
